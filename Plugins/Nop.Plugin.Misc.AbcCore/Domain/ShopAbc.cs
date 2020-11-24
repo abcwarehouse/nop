d7eae4b35ff5111d7cf31be1f567d372ddf09b88
Dave Farinelli <d@dfar.io>
2020-11-24 09:07:52 -0500

    Slight cleanup

--- Plugins/Nop.Plugin.Misc.AbcCore/Domain/ShopAbc.cs
+++ Plugins/Nop.Plugin.Misc.AbcCore/Domain/ShopAbc.cs
@@ -1,12 +1,4 @@
 using Nop.Core;
-using SevenSpikes.Nop.Plugins.StoreLocator.Domain.Shops;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nop.Plugin.Misc.AbcCore.Domain
 {
--- Plugins/Nop.Plugin.Misc.AbcCore/Models/ProductStock.cs
+++ Plugins/Nop.Plugin.Misc.AbcCore/Models/ProductStock.cs
@@ -1,9 +1,5 @@
 using SevenSpikes.Nop.Plugins.StoreLocator.Domain.Shops;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace Nop.Plugin.Misc.AbcCore.Models
