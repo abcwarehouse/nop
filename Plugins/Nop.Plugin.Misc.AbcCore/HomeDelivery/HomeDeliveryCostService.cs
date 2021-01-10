dabfc8ceb7ec47de14fa5e6e4af29e83dbbb9c3c
Dave Farinelli <d@dfar.io>
2021-01-10 13:43:43 -0500

    Calc Mattress Home Delivery Cost

--- Plugins/Nop.Plugin.Misc.AbcCore/HomeDelivery/HomeDeliveryCostService.cs
+++ Plugins/Nop.Plugin.Misc.AbcCore/HomeDelivery/HomeDeliveryCostService.cs
@@ -9,32 +9,37 @@ namespace Nop.Plugin.Misc.AbcCore.HomeDelivery
     public class HomeDeliveryCostService : IHomeDeliveryCostService
     {
         private readonly ICategoryService _categoryService;
+        private readonly IProductAttributeService _productAttributeService;
         private readonly IProductService _productService;
+        private readonly IProductAttributeParser _productAttributeParser;
 
         public HomeDeliveryCostService(
             ICategoryService categoryService,
-            IProductService productService
+            IProductAttributeService productAttributeService,
+            IProductService productService,
+            IProductAttributeParser productAttributeParser
         )
         {
             _categoryService = categoryService;
+            _productAttributeService = productAttributeService;
             _productService = productService;
+            _productAttributeParser = productAttributeParser;
         }
 
         public decimal GetHomeDeliveryCost(OrderItem orderItem)
         {
-            return GetCost(orderItem.ProductId);
+            return GetCost(orderItem.ProductId, orderItem.AttributesXml);
         }
 
         public decimal GetHomeDeliveryCost(GetShippingOptionRequest.PackageItem packageItem)
         {
-            return GetCost(packageItem.Product.Id);
+            return GetCost(packageItem.Product.Id, packageItem.ShoppingCartItem.AttributesXml);
         }
 
-        private decimal GetCost(int productId)
+        private decimal GetCost(int productId, string attributesXml)
         {
             var categories = _categoryService.GetProductCategoriesByProductId(productId)
                                                     .Select(pc => _categoryService.GetCategoryById(pc.CategoryId));
-
             if (categories.Any(c => new string[]
                 { "recliners", "lift chairs", "massage chairs" }.Contains(c.Name.ToLower()))
             )
@@ -42,8 +47,36 @@ namespace Nop.Plugin.Misc.AbcCore.HomeDelivery
                 return 49.00M;
             }
 
+            // Since that Package Item doens't carry the actual item 
+            if (categories.Any(c => new string[]
+                { "twin", "twin extra long", "full", "queen", "king", "california king" }
+                .Contains(c.Name.ToLower()))
+            )
+            {
+                return GetMattressCost(attributesXml);
+            }
+
             // default
             return 14.75M;
+        }
+
+        private decimal GetMattressCost(string attributesXml)
+        {
+            var mattressProductAttributeIds =
+                _productAttributeService.GetAllProductAttributes()
+                .Where(pa => pa.Name.Contains("Mattress Size") || pa.Name.Contains("Base ("))
+                .Select(pa => pa.Id);
+
+            var mattressItemCost =
+                _productAttributeParser.ParseProductAttributeMappings(attributesXml)
+                .Where(pam => mattressProductAttributeIds.Contains(pam.ProductAttributeId))
+                .Select(pam => _productAttributeParser.ParseValues(attributesXml, pam.Id).FirstOrDefault())
+                .Select(idasstring => _productAttributeService.GetProductAttributeValueById(int.Parse(idasstring)))
+                .Sum(pav => pav.PriceAdjustment);
+
+            return mattressItemCost >= 697.00M ?
+                0 :
+                59.95M;
         }
     }
 }
