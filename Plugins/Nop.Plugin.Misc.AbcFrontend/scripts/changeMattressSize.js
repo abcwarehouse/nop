5ff9223bdfa7f71f147013a3f61af1915cfc1296
Dave Farinelli <d@dfar.io>
2021-01-13 10:18:46 -0500

    Added select base from URL

--- Plugins/Nop.Plugin.Misc.AbcFrontend/scripts/changeMattressSize.js
+++ Plugins/Nop.Plugin.Misc.AbcFrontend/scripts/changeMattressSize.js
@@ -3,7 +3,7 @@ function changeMattressSize()
     // Get the size value from URL if it exists
     const urlParams = new URLSearchParams(window.location.search);
     const sizeValue = urlParams.get('size');
-    if (sizeValue == null || !isValidSize(sizeValue)) { return; }
+    if (sizeValue == null || !isValidBase(sizeValue)) { return; }
 
     // Find the matching option based on size above
     // SPecial case for TwinXL
@@ -19,10 +19,58 @@ function changeMattressSize()
     matchingElement.parentNode.dispatchEvent(new Event('change'));
 }
 
-function isValidSize(size)
+function changeMattressBase()
+{
+    // Get the base value from URL if it exists
+    const urlParams = new URLSearchParams(window.location.search);
+    const baseValue = urlParams.get('base');
+    if (baseValue == null || !isValidBase(baseValue)) { return; }
+
+    // Find the matching options based on base above (must check all bases)
+    var textToFind = getConvertedBase(baseValue);
+    var xpath = `//option[contains(text(),'${textToFind}')]`;
+    var matchingElements = getElementsByXPath(xpath);
+    if (matchingElements.length === 0) { return; }
+
+    // Change the values and kick off change events
+    matchingElements.forEach(element => {
+        element.parentNode.value = element.value;
+        element.parentNode.dispatchEvent(new Event('change'));
+    });
+}
+
+function isValidBase(size)
 {
     const validSizes = ["twin", "twinxl", "full", "queen", "king", "california"];
     return validSizes.includes(size.toLowerCase());
 }
 
+function isValidBase(base)
+{
+    const validBases = ["lowprofile", "ease", "regular", "ergo", "ergoextended"];
+    return validBases.includes(base.toLowerCase());
+}
+
+function getConvertedBase(baseValue) {
+    switch (baseValue) {
+        case 'lowprofile':
+            return "Low Profile";
+        case 'ergoextended':
+                return "Ergo Extended";
+        default:
+            return baseValue.toLowerCase().charAt(0).toUpperCase() + baseValue.slice(1);
+    }
+}
+
+function getElementsByXPath(xpath) {
+    let results = [];
+    let query = document.evaluate(xpath, document,
+        null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (let i = 0, length = query.snapshotLength; i < length; ++i) {
+        results.push(query.snapshotItem(i));
+    }
+    return results;
+}
+
 changeMattressSize();
+changeMattressBase();
