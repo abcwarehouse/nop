2831d064edd5b1e91f576c5ae2238ccf14094255
Dave Farinelli <d@dfar.io>
2021-02-23 11:30:33 -0500

    Enables attribute-based prices in term lookup

--- Plugins/Nop.Plugin.Misc.AbcCore/Isam/TermLookupService.cs
+++ Plugins/Nop.Plugin.Misc.AbcCore/Isam/TermLookupService.cs
@@ -8,6 +8,7 @@ using Nop.Core.Domain.Orders;
 using Nop.Services.Catalog;
 using Nop.Services.Logging;
 using Nop.Core.Domain.Logging;
+using System.Linq;
 
 namespace Nop.Plugin.Misc.AbcCore.Services
 {
@@ -16,16 +17,19 @@ namespace Nop.Plugin.Misc.AbcCore.Services
         private readonly IProductService _productService;
         private readonly ILogger _logger;
         private readonly CoreSettings _settings;
+        private readonly IProductAttributeParser _productAttributeParser;
 
         public TermLookupService(
             IProductService productService,
             ILogger logger,
-            CoreSettings settings
+            CoreSettings settings,
+            IProductAttributeParser productAttributeParser
         )
         {
             _productService = productService;
             _logger = logger;
             _settings = settings;
+            _productAttributeParser = productAttributeParser;
         }
 
         public (string termNo, string description, string link) GetTerm(
@@ -44,11 +48,14 @@ namespace Nop.Plugin.Misc.AbcCore.Services
             foreach (var item in cart)
             {
                 var product = _productService.GetProductById(item.ProductId);
+                var priceAdjustment = _productAttributeParser.ParseProductAttributeValues(item.AttributesXml)
+                                                            .Sum(p => p.PriceAdjustment);
+                var price = product.Price + priceAdjustment;
                 xml += $"{nl}<Item>{nl}<Sku>{product.Sku}</Sku>{nl}" +
                        $"<Gtin>{product.Gtin}</Gtin>{nl}" +
                        $"<Qty>{item.Quantity}</Qty>{nl}" +
                        $"<Brand>{product.Name}</Brand>{nl}" +
-                       $"<Price>{product.Price}</Price>{nl}</Item>";
+                       $"<Price>{price}</Price>{nl}</Item>";
             }
             xml += $"{nl}</Items>{nl}</Term_Lookup>{nl}</Request>";
 
