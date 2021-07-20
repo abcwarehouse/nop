c5b7d3ff6d5c683e213012f72b021a263cdb5977
Dave Farinelli <d@dfar.io>
2021-07-20 22:49:41 +0000

    Adds SearchPageRedirect

---
+++ src/Plugins/Nop.Plugin.Misc.SearchPageRedirect/SearchPageRedirectPlugin.cs
@@ -0,0 +1,23 @@
+﻿using Nop.Core;
+using Nop.Services.Common;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.SearchPageRedirect
+{
+    public class SearchPageRedirectPlugin : BasePlugin, IMiscPlugin
+    {
+        private readonly IWebHelper _webHelper;
+
+        public SearchPageRedirectPlugin(
+            IWebHelper webHelper
+        )
+        {
+            _webHelper = webHelper;
+        }
+
+        public override string GetConfigurationPageUrl()
+        {
+            return $"{_webHelper.GetStoreLocation()}Admin/SearchPageRedirect/Configure";
+        }
+    }
+}
