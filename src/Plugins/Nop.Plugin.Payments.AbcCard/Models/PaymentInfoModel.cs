071b9fa4cad0834e9115b6a259e9e88ac8641b28
Dave Farinelli <d@dfar.io>
2021-07-20 23:51:09 +0000

    Started on AbcCard

---
+++ src/Plugins/Nop.Plugin.Payments.AbcCard/Models/PaymentInfoModel.cs
@@ -0,0 +1,17 @@
+﻿using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nop.Plugin.Payments.AbcCard.Models
+{
+    public record PaymentInfoModel : BaseNopModel
+    {
+        public string DescriptionText { get; set; }
+
+        [NopResourceDisplayName("Payment.CardNumber")]
+        [Required]
+        [CreditCard]
+        public string CardNumber { get; set; }
+
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Payments.AbcCard/Validators/PaymentInfoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Nop.Plugin.Payments.AbcCard.Models;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Plugin.Payments.AbcCard.Validators
+{
+    public partial class PaymentInfoValidator : BaseNopValidator<PaymentInfoModel>
+    {
+        public PaymentInfoValidator(ILocalizationService localizationService)
+        {
+            //useful links:
+            //http://fluentvalidation.codeplex.com/wikipage?title=Custom&referringTitle=Documentation&ANCHOR#CustomValidator
+            //http://benjii.me/2010/11/credit-card-validator-attribute-for-asp-net-mvc-3/
+
+            //RuleFor(x => x.CardNumber).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardNumber.Required"));
+            //RuleFor(x => x.CardCode).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardCode.Required"));
+
+            RuleFor(x => x.CardNumber).IsCreditCard().WithMessage(localizationService.GetResource("Payment.CardNumber.Wrong"));
+        }
+    }
+}
