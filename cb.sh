a812cbb4c7c5c598c97da39d5758525a708a04ed
Dave Farinelli <d@dfar.io>
2021-09-22 16:35:05 +0000

    Adds AjaxCart plugin

--- cb.sh
+++
@@ -1,6 +0,0 @@
-# For linux users
-# clears the Windows generated custom slider resources, then clean builds.
-
-rm -rf src/Presentation/Nop.Web/wwwroot\\css*
-dotnet clean src/NopCommerce.sln
-dotnet build src/NopCommerce.sln
