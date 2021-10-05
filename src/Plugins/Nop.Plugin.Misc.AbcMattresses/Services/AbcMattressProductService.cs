7dc8386d877d4c0eacecaba983c62074ce58dc29
Dave Farinelli <d@dfar.io>
2021-10-05 13:37:29 +0000

    Generalize mattress store matching

--- src/Plugins/Nop.Plugin.Misc.AbcMattresses/Services/AbcMattressProductService.cs
+++ src/Plugins/Nop.Plugin.Misc.AbcMattresses/Services/AbcMattressProductService.cs
@@ -276,18 +276,18 @@ namespace Nop.Plugin.Misc.AbcMattresses.Services
 
         private async Task MapProductToStoreAsync(Product product)
         {
-            // hardcoded to ABC Warehouse currently
-            var abcWarehouseStore = (await _storeService.GetAllStoresAsync())
-                                                   .Where(s => s.Name == "ABC Warehouse")
+            // get the non-clearance store
+            var store = (await _storeService.GetAllStoresAsync())
+                                                   .Where(s => !s.Name.Contains("Clearance"))
                                                    .FirstOrDefault();
-            if (abcWarehouseStore == null)
+            if (store == null)
             {
-                throw new Exception("Unable to find ABC Warehouse store.");
+                throw new Exception("Unable to find non-clearance store.");
             }
 
             await _productService.UpdateProductStoreMappingsAsync(
                 product,
-                new int[] { abcWarehouseStore.Id }
+                new int[] { store.Id }
             );
         }
 
