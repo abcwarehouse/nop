a20d5439f22aa027c71bf2a8c3f6fb9768c89e41
Dave Farinelli <d@dfar.io>
2022-01-19 17:08:03 +0000

    Removes /tests

--- tests/homepage.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('can open homepage', async ({ page }) => {
-    await page.goto('/');
-    await page.waitForSelector('.home-page-category-grid');
-    const categoryGrids = page.$$('.category-item');
-    expect((await categoryGrids).length).toBeCloseTo(8);
-});
--- tests/playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import { PlaywrightTestConfig } from '@playwright/test';
-
-const config: PlaywrightTestConfig = {
-    use: {
-        baseURL: "https://www.abcwarehouse.com"
-    }
-};
-
-export default config;
