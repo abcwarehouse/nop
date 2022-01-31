3e7e8d29b45908f30b1e164a4e434239b9505f63
Dave Farinelli <d@dfar.io>
2022-01-31 20:51:01 +0000

    Adds messaging for failed AbcDeliveryItem lookup

--- src/Plugins/AbcWarehouse.Plugin.Widgets.CartSlideout/Services/AbcDeliveryService.cs
+++ src/Plugins/AbcWarehouse.Plugin.Widgets.CartSlideout/Services/AbcDeliveryService.cs
@@ -1,27 +1,40 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AbcWarehouse.Plugin.Widgets.CartSlideout.Domain;
 using Nop.Data;
+using Nop.Services.Logging;
 
 namespace AbcWarehouse.Plugin.Widgets.CartSlideout.Services
 {
     public class AbcDeliveryService : IAbcDeliveryService
     {
+        private readonly ILogger _logger;
         private readonly IRepository<AbcDeliveryItem> _abcDeliveryItemRepository;
         private readonly IRepository<AbcDeliveryMap> _abcDeliveryMapRepository;
 
         public AbcDeliveryService(
+            ILogger logger,
             IRepository<AbcDeliveryItem> abcDeliveryItemRepository,
             IRepository<AbcDeliveryMap> abcDeliveryMapRepository)
         {
+            _logger = logger;
             _abcDeliveryItemRepository = abcDeliveryItemRepository;
             _abcDeliveryMapRepository = abcDeliveryMapRepository;
         }
 
         public async Task<AbcDeliveryItem> GetAbcDeliveryItemByItemNumberAsync(int itemNumber)
         {
-            return await _abcDeliveryItemRepository.Table.SingleAsync(adi => adi.Item_Number == itemNumber);
+            try
+            {
+                return await _abcDeliveryItemRepository.Table.SingleAsync(adi => adi.Item_Number == itemNumber);
+            }
+            catch (AggregateException)
+            {
+                await _logger.ErrorAsync($"Cannot find single AbcDeliveryItem with ItemNumber {itemNumber}");
+                throw;
+            }
         }
 
         public async Task<IList<AbcDeliveryMap>> GetAbcDeliveryMapsAsync()
